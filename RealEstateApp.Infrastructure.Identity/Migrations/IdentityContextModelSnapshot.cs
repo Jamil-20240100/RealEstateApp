552cab3793939b9e9d603a79288b6c09bb835b8d
Jamil Guzmán <jamilguzman202@gmail.com>
2025-08-16 01:49:59 -0400

    modified views done

--- RealEstateApp.Infrastructure.Identity/Migrations/IdentityContextModelSnapshot.cs
+++ RealEstateApp.Infrastructure.Identity/Migrations/IdentityContextModelSnapshot.cs
@@ -18,7 +18,7 @@ namespace RealEstateApp.Infrastructure.Identity.Migrations
 #pragma warning disable 612, 618
             modelBuilder
                 .HasDefaultSchema("Identity")
-                .HasAnnotation("ProductVersion", "9.0.7")
+                .HasAnnotation("ProductVersion", "9.0.8")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
